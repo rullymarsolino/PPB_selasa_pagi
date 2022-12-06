2825d6ac618a78ac59002c5f18c2c7f9d7513961
rullymarsolino <rullymarselino005@gmail.com>
2022-12-06 11:27:11 +0700

    Pertemuan10

--- latihan_project/lib/main.dart
+++ latihan_project/lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'pages/myactionbutton.dart';
+import 'pages/mybottombar.dart';
+import 'pages/foodlistview.dart';
+import 'pages/selecttypesection.dart';
+import 'pages/menuitemlist.dart';
+import 'pages/myappbar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,33 +18,33 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Project UI',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        
         primarySwatch: Colors.blue,
       ),
       home: ProjectPage(),
     );
   }
 }
-class ProjectPage extends StatelessWidget {
 
+class ProjectPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: MyActionButton,
+      floatingActionButton: MyActionButton(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: MyBottomBar(),
       body: Container(
-        child:ListView(
+        child: ListView(
           children: [
             SizedBox(height: 16.0,),
-            MyApp(),
+            MyAppBar(),
             SizedBox(height: 16.0,),
             FoodListView(),
             SizedBox(height: 16.0,),
             SelectTypeSection(),
             SizedBox(height: 16.0,),
-            MenuItemlist(),
+            MenuItemsList(),
           ],
         ),
       ),
